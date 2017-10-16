41d509d2aaed5b249259dd9d5fe5d0e765579d4f
Kalive Purushotham Kumar <pkkalive@users.noreply.github.com>
2017-10-17 03:35:05 +0530

    removed all unwanted code and starting the project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="new">
+        <p align="center">{this.props.text}</p>
+          <div id="mynew">
+              <ul>
+                  <li>{this.props.children}</li>
+                  <li>Item 2</li>
+                  <li>Item 3</li>
+              </ul>
+          </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<div>
+	<App text="Hello World"> This is first component </App>
+	<App text="I am a component"> This is second component </App>
+	<App text="I have been reused !!">  This is third component </App>
+	</div>, document.getElementById('root'));
